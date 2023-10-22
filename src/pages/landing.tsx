a15739d2164e3b55f5468431615631246bf84abc
Beginner-Project <76930515+kaomao1234@users.noreply.github.com>
2023-10-22 14:01:02 +0000

    init

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,8 +1,11 @@
 export default function Landing() {
-    return <div id="container" className="w-full h-full flex justify-center items-center">
-        <div className="navbar space-x-3 flex">
-            <h1>live safety</h1>
-            <h1>live safety</h1>
+    return <div id="container" className="w-full h-full flex bg-base-100 flex-col min-h-screen">
+        <div className="navbar space-x-3 flex justify-center bg-black h-14">
+            <label className="text-red-400 rounded  flex items-center justify-center text-2xl">Master of Shadow</label>
+        </div>
+        <div id="body" className="flex flex-col">
+            <label className="flex justify-center capitalize text-2xl m-5">The eminence shadow</label>
+            <div id="img" className="min-w-screen min-h-[13rem] mx-3 bg-white"></div>
         </div>
     </div>
 }
